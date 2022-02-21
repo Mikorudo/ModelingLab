31c6bd323a8f84084f6f17a60db7760d7795d2a3
Михаил Косточкин <kostochkin2002@inbox.ru>
2022-02-21 11:43:56 +0700

    Добавьте файлы проекта.

---
+++ Graphic/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace Graphic
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+			this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Location = new System.Drawing.Point(56, 79);
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+			this.numericUpDown1.TabIndex = 0;
+			// 
+			// numericUpDown2
+			// 
+			this.numericUpDown2.Location = new System.Drawing.Point(56, 130);
+			this.numericUpDown2.Name = "numericUpDown2";
+			this.numericUpDown2.Size = new System.Drawing.Size(120, 22);
+			this.numericUpDown2.TabIndex = 1;
+			// 
+			// numericUpDown3
+			// 
+			this.numericUpDown3.Location = new System.Drawing.Point(56, 173);
+			this.numericUpDown3.Name = "numericUpDown3";
+			this.numericUpDown3.Size = new System.Drawing.Size(120, 22);
+			this.numericUpDown3.TabIndex = 2;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(56, 56);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(106, 17);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "Левая граница";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(56, 110);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(114, 17);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Правая граница";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(56, 155);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(51, 17);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "Лямда";
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.ItemHeight = 16;
+			this.listBox1.Location = new System.Drawing.Point(59, 265);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(224, 212);
+			this.listBox1.TabIndex = 6;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(59, 217);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(140, 28);
+			this.button1.TabIndex = 7;
+			this.button1.Text = "Сгенерировать";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// chart1
+			// 
+			chartArea1.Name = "ChartArea1";
+			this.chart1.ChartAreas.Add(chartArea1);
+			legend1.Name = "Legend1";
+			this.chart1.Legends.Add(legend1);
+			this.chart1.Location = new System.Drawing.Point(289, 12);
+			this.chart1.Name = "chart1";
+			this.chart1.Size = new System.Drawing.Size(718, 465);
+			this.chart1.TabIndex = 8;
+			this.chart1.Text = "chart1";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1041, 516);
+			this.Controls.Add(this.chart1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.numericUpDown3);
+			this.Controls.Add(this.numericUpDown2);
+			this.Controls.Add(this.numericUpDown1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.NumericUpDown numericUpDown2;
+		private System.Windows.Forms.NumericUpDown numericUpDown3;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+	}
+}
+
---
+++ Graphic/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graphic
+{
+    public partial class Form1 : Form
+    {
+        static Random rnd = new Random();
+        static double c; //левая граница
+        static double d; //правая граница
+        static double lambda; //лямбда
+        List<double> Selection;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double F(double x, double lambda)
+        {
+            return lambda * x - Math.Pow(lambda, 2) * Math.Pow(x, 2) / 4;
+        }
+        double GenerateRandomVariable(double lambda, double cy, double dy)
+        {
+            double y = cy + rnd.NextDouble() * (dy - cy);
+            return 2 * (1 - Math.Sqrt(1 - y)) / lambda;
+        }
+        List<double> GenerateSelection(double amount, double c, double d, double lambda)
+        {
+            double cy, dy;
+            if (d < c)
+                (d, c) = (c, d); //смена границ если d < c
+
+            if (c < 0)
+                cy = F(0, lambda);
+            else
+                cy = F(c, lambda);
+
+            if (d < 0)
+                dy = F(0, lambda);
+            else
+                dy = F(d, lambda);
+
+            List<double> Selection = new List<double>();
+            for (int i = 0; i < amount; i++)
+            {
+                Selection.Add(GenerateRandomVariable(lambda, cy, dy));
+            }
+            return Selection;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            c = 0.5; //c = numericUpDown...
+            d = 1.5;
+            lambda = 1;
+            Selection = GenerateSelection(1000, c, d, lambda);
+            foreach (double d in Selection)
+            {
+                listBox1.Items.Add(d);
+            }
+
+
+
+
+            //GraphicPoints = new List<PointF>();
+            chart1.Series.Add("Эксперементальная функция");
+            chart1.Series["Эксперементальная функция"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            int size = 1000;
+			for (int i = 0; i < size; i++)
+			{
+                float step = Convert.ToSingle((d - c)/(size/*-1*/));
+                float x = Convert.ToSingle(c + step * i);
+                float y = Selection.Where(p => p < x).Count() / Selection.Count;
+                //GraphicPoints.Add(new PointF(x, y));
+                chart1.Series["Эксперементальная функция"].Points.AddXY(x, y);
+			}
+
+        }
+    }
+}
