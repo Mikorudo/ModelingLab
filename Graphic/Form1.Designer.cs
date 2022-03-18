d084ba9ac7febd762ddf1fd814e18be8d21364ab
Михаил Косточкин <kostochkin2002@inbox.ru>
2022-03-18 14:24:14 +0700

    v 1.0

--- Graphic/Form1.Designer.cs
+++ Graphic/Form1.Designer.cs
@@ -29,8 +29,8 @@ namespace Graphic
 		/// </summary>
 		private void InitializeComponent()
 		{
-			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-			System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
 			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
 			this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
 			this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
@@ -40,37 +40,81 @@ namespace Graphic
 			this.listBox1 = new System.Windows.Forms.ListBox();
 			this.button1 = new System.Windows.Forms.Button();
 			this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.label4 = new System.Windows.Forms.Label();
+			this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
 			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// numericUpDown1
 			// 
-			this.numericUpDown1.Location = new System.Drawing.Point(56, 79);
+			this.numericUpDown1.DecimalPlaces = 3;
+			this.numericUpDown1.Location = new System.Drawing.Point(68, 39);
+			this.numericUpDown1.Maximum = new decimal(new int[] {
+            99999,
+            0,
+            0,
+            0});
+			this.numericUpDown1.Minimum = new decimal(new int[] {
+            99999,
+            0,
+            0,
+            -2147483648});
 			this.numericUpDown1.Name = "numericUpDown1";
 			this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
 			this.numericUpDown1.TabIndex = 0;
+			this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
 			// 
 			// numericUpDown2
 			// 
-			this.numericUpDown2.Location = new System.Drawing.Point(56, 130);
+			this.numericUpDown2.DecimalPlaces = 3;
+			this.numericUpDown2.Location = new System.Drawing.Point(68, 90);
+			this.numericUpDown2.Maximum = new decimal(new int[] {
+            99999,
+            0,
+            0,
+            0});
+			this.numericUpDown2.Minimum = new decimal(new int[] {
+            99999,
+            0,
+            0,
+            -2147483648});
 			this.numericUpDown2.Name = "numericUpDown2";
 			this.numericUpDown2.Size = new System.Drawing.Size(120, 22);
 			this.numericUpDown2.TabIndex = 1;
+			this.numericUpDown2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
 			// 
 			// numericUpDown3
 			// 
-			this.numericUpDown3.Location = new System.Drawing.Point(56, 173);
+			this.numericUpDown3.DecimalPlaces = 3;
+			this.numericUpDown3.Location = new System.Drawing.Point(68, 133);
+			this.numericUpDown3.Maximum = new decimal(new int[] {
+            99999,
+            0,
+            0,
+            0});
+			this.numericUpDown3.Minimum = new decimal(new int[] {
+            99999,
+            0,
+            0,
+            -2147483648});
 			this.numericUpDown3.Name = "numericUpDown3";
 			this.numericUpDown3.Size = new System.Drawing.Size(120, 22);
 			this.numericUpDown3.TabIndex = 2;
+			this.numericUpDown3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+			this.numericUpDown3.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
 			// 
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(56, 56);
+			this.label1.Location = new System.Drawing.Point(68, 16);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(106, 17);
 			this.label1.TabIndex = 3;
@@ -79,7 +123,7 @@ namespace Graphic
 			// label2
 			// 
 			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(56, 110);
+			this.label2.Location = new System.Drawing.Point(68, 70);
 			this.label2.Name = "label2";
 			this.label2.Size = new System.Drawing.Size(114, 17);
 			this.label2.TabIndex = 4;
@@ -88,7 +132,7 @@ namespace Graphic
 			// label3
 			// 
 			this.label3.AutoSize = true;
-			this.label3.Location = new System.Drawing.Point(56, 155);
+			this.label3.Location = new System.Drawing.Point(68, 115);
 			this.label3.Name = "label3";
 			this.label3.Size = new System.Drawing.Size(51, 17);
 			this.label3.TabIndex = 5;
@@ -111,25 +155,59 @@ namespace Graphic
 			this.button1.TabIndex = 7;
 			this.button1.Text = "Сгенерировать";
 			this.button1.UseVisualStyleBackColor = true;
-			this.button1.Click += new System.EventHandler(this.button1_Click);
+			this.button1.Click += new System.EventHandler(this.Start);
 			// 
 			// chart1
 			// 
-			chartArea2.Name = "ChartArea1";
-			this.chart1.ChartAreas.Add(chartArea2);
-			legend2.Name = "Legend1";
-			this.chart1.Legends.Add(legend2);
+			chartArea1.Name = "ChartArea1";
+			this.chart1.ChartAreas.Add(chartArea1);
+			legend1.Name = "Legend1";
+			this.chart1.Legends.Add(legend1);
 			this.chart1.Location = new System.Drawing.Point(289, 12);
 			this.chart1.Name = "chart1";
 			this.chart1.Size = new System.Drawing.Size(718, 465);
 			this.chart1.TabIndex = 8;
 			this.chart1.Text = "chart1";
 			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(68, 162);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(117, 17);
+			this.label4.TabIndex = 10;
+			this.label4.Text = "Размер выборки";
+			// 
+			// numericUpDown4
+			// 
+			this.numericUpDown4.Location = new System.Drawing.Point(68, 180);
+			this.numericUpDown4.Maximum = new decimal(new int[] {
+            999999,
+            0,
+            0,
+            0});
+			this.numericUpDown4.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			this.numericUpDown4.Name = "numericUpDown4";
+			this.numericUpDown4.Size = new System.Drawing.Size(120, 22);
+			this.numericUpDown4.TabIndex = 9;
+			this.numericUpDown4.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+			this.numericUpDown4.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(1041, 516);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.numericUpDown4);
 			this.Controls.Add(this.chart1);
 			this.Controls.Add(this.button1);
 			this.Controls.Add(this.listBox1);
@@ -145,6 +223,7 @@ namespace Graphic
 			((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -161,6 +240,8 @@ namespace Graphic
 		private System.Windows.Forms.ListBox listBox1;
 		private System.Windows.Forms.Button button1;
 		private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.NumericUpDown numericUpDown4;
 	}
 }
 
--- Graphic/Form1.cs
+++ Graphic/Form1.cs
@@ -10,13 +10,12 @@ using System.Windows.Forms;
 
 namespace Graphic
 {
+    //Левая граница
+    //Условие Пирсона
+    //Пара
     public partial class Form1 : Form
     {
         static Random rnd = new Random();
-        static double c; //левая граница
-        static double d; //правая граница
-        static double lambda; //лямбда
-        List<double> Selection;
         public Form1()
         {
             InitializeComponent();
@@ -27,84 +26,97 @@ namespace Graphic
         }
         double GenerateRandomVariable(double lambda, double cy, double dy)
         {
-            //double y = cy + rnd.NextDouble() * (dy - cy);
-            double y = rnd.NextDouble();
+            double y = cy + rnd.NextDouble() * (dy - cy);
             return 2 * (1 - Math.Sqrt(1 - y)) / lambda;
         }
-        List<double> GenerateSelection(double amount, double left, double right, double lambda)
+        List<double> GenerateSelection(int amount, double left, double right, double lambda)
         {
-            double cy, dy;
+            double leftProbability, rightProbability;
             if (right < left)
-                (right, left) = (left, right); //смена границ
+                throw new Exception("right border less than left border");
+
             if (lambda > 0)
             {
                 if (left < 0)
-                    cy = 0;
+                    leftProbability = 0;
                 else
-                    cy = F(left, lambda);
+                    leftProbability = F(left, lambda);
             }
             else if (lambda < 0)
             {
-                if (left < -lambda)
-                    cy = 0;
+                if (left < 4/lambda)
+                    leftProbability = 0;
                 else
-                    cy = F(left, lambda);
+                    leftProbability = F(left, lambda);
             }
             else
                 throw new Exception("lambda = 0");
             
 
             if (right > 2/lambda)
-                dy = 1;
+                rightProbability = 1;
             else
-                dy = F(right, lambda);
+                rightProbability = F(right, lambda);
 
             List<double> Selection = new List<double>();
             for (int i = 0; i < amount; i++)
             {
-                Selection.Add(GenerateRandomVariable(lambda, cy, dy));
+                Selection.Add(GenerateRandomVariable(lambda, leftProbability, rightProbability));
             }
             return Selection;
         }
-        private void button1_Click(object sender, EventArgs e)
+        private void Start(object sender, EventArgs e)
         {
-            c = 0; //c = numericUpDown...
-            d = 2;
-            lambda = 1;
-            int selectionSize = 1000;
-            Selection = GenerateSelection(selectionSize, c, d, lambda);
-            foreach (double d in Selection)
-            {
-                listBox1.Items.Add(d);
+            if (numericUpDown3.Value == 0)
+			{
+                MessageBox.Show("Лямбда равна 0!");
+                return;
+			}
+			if (numericUpDown2.Value <= numericUpDown1.Value)
+			{
+                MessageBox.Show("Правая граница меньше левой!");
+                return;
             }
-            //GraphicPoints = new List<PointF>();
+
+            double left = Convert.ToDouble(numericUpDown1.Value);
+            double right = Convert.ToDouble(numericUpDown2.Value);
+            double lambda = Convert.ToDouble(numericUpDown3.Value);
+            int selectionSize = Convert.ToInt32(numericUpDown4.Value); ;
+            List<double> Selection = GenerateSelection(selectionSize, left, right, lambda);
+            listBox1.Items.Clear();
+            foreach (double d in Selection)
+                listBox1.Items.Add(d);
+            #region Параметры графика
+            chart1.Series.Clear();
             chart1.Series.Add("Эксперементальная функция");
-            chart1.Series["Эксперементальная функция"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            chart1.ChartAreas[0].AxisX.Minimum = 0;
-            chart1.ChartAreas[0].AxisX.Maximum = 2;
+            chart1.Series["Эксперементальная функция"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            chart1.ChartAreas[0].AxisX.Minimum = Convert.ToDouble(numericUpDown1.Value);
+            chart1.ChartAreas[0].AxisX.Maximum = Convert.ToDouble(numericUpDown2.Value);
             chart1.ChartAreas[0].AxisY.Minimum = 0;
             chart1.ChartAreas[0].AxisY.Maximum = 1;
             chart1.ChartAreas[0].BorderWidth = 4;
             chart1.Series["Эксперементальная функция"].Color = Color.Black;
-            List<PointF> points = new List<PointF>();
+            #endregion
+
             int chartSize = 100; 
 			for (int i = 0; i < chartSize; i++)
 			{
-                float step = Convert.ToSingle((d - c)/(chartSize));
-                float x = Convert.ToSingle(c + step * i);
-                int count = 0;
-				for (int j = 0; j < selectionSize; j++)
-				{
-                    if (Selection[j] <= x)
-                        count++;
-				}
-                float y = (float)count / selectionSize;
-                points.Add(new PointF(x, y));
+                float step = Convert.ToSingle((right - left)/(chartSize));
+                float x = Convert.ToSingle(left + step * i);
+                float y = (float)Selection.Where(p => p < x).Count() / Selection.Count;
+                //int count = 0;
+				//for (int j = 0; j < selectionSize; j++)
+				//{
+                //    if (Selection[j] <= x)
+                //        count++;
+				//}
+
+                //float y = (float)count / selectionSize;
+                chart1.Series["Эксперементальная функция"].Points.AddXY(x, y);
 			}
 			for (int i = 0; i < chartSize; i++)
 			{
-                chart1.Series["Эксперементальная функция"].Points.AddXY(points[i].X, points[i].Y);
 			}
         }
-    }
+	}
 }
