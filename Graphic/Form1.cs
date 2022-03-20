4b8b0fb221b7c312bc83c1cc63438b0b9843c9f3
Михаил Косточкин <kostochkin2002@inbox.ru>
2022-03-20 19:54:05 +0700

    Почти конец

--- Graphic/Form1.cs
+++ Graphic/Form1.cs
@@ -10,144 +10,136 @@ using System.Windows.Forms;
 
 namespace Graphic
 {
-    //Левая граница
-    //Условие Пирсона
-    //Пара
-
     public partial class Form1 : Form
     {
-        static Random rnd = new Random();
         public Form1()
         {
             InitializeComponent();
         }
-        //F(x) = Интеграл всех значение от -бесконечности до x 
-        double F(double x, double lambda)
-        {
-            if (x >= 2 / lambda)
-                return 1;
-            if (x <= 0) //поменять условие для lamda < 0
-                return 0;
-            return lambda * x - Math.Pow(lambda, 2) * Math.Pow(x, 2) / 4; // -l^2*x^2 / 4 + l*x - r = 0;
-            // D = l^2 - 4 * (-l^2 / 4) * (-r)
-            //x1 = ((-l) - Корень(D)) / 2 -l^2
-        }
-        double GenerateRandomVariable(double lambda, double cy, double dy, double rand0_1)
-        {
-            double y = cy + rand0_1 * (dy - cy);
-            return 2 * (1 - Math.Sqrt(1 - y)) / lambda;
-        }
-        List<double> GenerateSelection(int amount, double left, double right, double lambda)
-        {
-            double leftProbability, rightProbability;
-            if (right < left)
-                throw new Exception("right border less than left border");
-
-            if (lambda > 0)
-            {
-                if (left < 0)
-                    leftProbability = 0;
-                else
-                    leftProbability = F(left, lambda);
-            }
-            else if (lambda < 0)
-            {
-                if (left < 4/lambda)
-                    leftProbability = 0;
-                else
-                    leftProbability = F(left, lambda);
-            }
-            else
-                throw new Exception("lambda = 0");
-            
-
-            if (right > 2/lambda)
-                rightProbability = 1;
-            else
-                rightProbability = F(right, lambda);
-
-            if (leftProbability == rightProbability)
-                throw new Exception("На этом интервале не генерируется СВ");
-
-            List<double> Selection = new List<double>();
-            for (int i = 0; i < amount; i++)
-            {
-                Selection.Add(GenerateRandomVariable(lambda, leftProbability, rightProbability, rnd.NextDouble()));
-            }
-            return Selection;
-        }
-        private void Start(object sender, EventArgs e)
-        {
+        
+        private void GenerateAndPrint()
+		{
             if (numericUpDown3.Value == 0)
-			{
-                MessageBox.Show("Лямбда равна 0!");
-                return;
-			}
-			if (numericUpDown2.Value <= numericUpDown1.Value)
-			{
-                MessageBox.Show("Правая граница меньше левой!");
-                return;
-            }
+                throw new Exception("Лямбда равна 0!");
+            if (numericUpDown2.Value <= numericUpDown1.Value)
+                throw new Exception("Правая граница меньше левой!");
+
 
             double left = Convert.ToDouble(numericUpDown1.Value);
             double right = Convert.ToDouble(numericUpDown2.Value);
             double lambda = Convert.ToDouble(numericUpDown3.Value);
             int selectionSize = Convert.ToInt32(numericUpDown4.Value); ;
-            List<double> Selection = GenerateSelection(selectionSize, left, right, lambda);
+            List<double> Selection = LambdaRandVarGenerator.GenerateSelection(selectionSize, left, right, lambda);
             listBox1.Items.Clear();
-            foreach (double d in Selection)
-                listBox1.Items.Add(d);
             #region Параметры графика
             chart1.Series.Clear();
             chart1.Series.Add("Эксперементальная функция");
             chart1.Series["Эксперементальная функция"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            chart1.ChartAreas[0].AxisX.Minimum = Convert.ToDouble(numericUpDown1.Value);
-            chart1.ChartAreas[0].AxisX.Maximum = Convert.ToDouble(numericUpDown2.Value);
+            chart1.Series["Эксперементальная функция"].Color = Color.Red;
+            chart1.Series.Add("Теоритическая функция");
+            chart1.Series["Теоритическая функция"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            chart1.Series["Теоритическая функция"].Color = Color.Blue;
+            chart1.Series.Add("Критерий Колмогорова");
+            chart1.Series["Критерий Колмогорова"].BorderWidth = 2;
+            chart1.Series["Критерий Колмогорова"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            chart1.Series["Критерий Колмогорова"].Color = Color.Green;
+            chart1.ChartAreas[0].AxisX.Minimum = left;
+            chart1.ChartAreas[0].AxisX.Maximum = right;
             chart1.ChartAreas[0].AxisY.Minimum = 0;
             chart1.ChartAreas[0].AxisY.Maximum = 1;
             chart1.ChartAreas[0].BorderWidth = 4;
-            chart1.Series["Эксперементальная функция"].Color = Color.Black;
+            #endregion
+            int chartSize = 100; //Количество точек для построения графика
+            for (int i = 0; i < chartSize; i++)
+            {
+                float step = Convert.ToSingle((right - left) / (chartSize));
+                float x = Convert.ToSingle(left + step * i);
+
+                float y = Convert.ToSingle(LambdaRandVarGenerator.FinRange(x, lambda, left, right));
+                chart1.Series["Теоритическая функция"].Points.AddXY(x, y);
+
+                y = (float)Selection.Where(p => p <= x).Count() / Selection.Count;
+                chart1.Series["Эксперементальная функция"].Points.AddXY(x, y);
+            }
+
+            #region Критерий Коломогорова
+            //1. Сортируем
+            Selection.Sort();
+            //2. Перебираем элементы в порядке возрастания и сравниваем |Fэкспер. - Fтеор.|, находим максимум этой разности
+            double xMaxDif = Selection[0];
+            double yMaxDif = 0;
+            double difMax = -1;
+            for (int i = 0; i < Selection.Count; i++)
+            {
+                double Fteor = LambdaRandVarGenerator.FinRange(Selection[i], lambda, left, right);
+                double Fexp = (double)(i + 1) / Selection.Count;
+                double dif = Math.Abs(Fteor - Fexp);
+                if (dif >= difMax)
+                {
+                    difMax = dif;
+                    xMaxDif = Selection[i];
+                    yMaxDif = Fexp;
+                }
+            }
+            //3. Выводим результат
+            chart1.Series["Критерий Колмогорова"].Points.AddXY(xMaxDif, yMaxDif);
+            chart1.Series["Критерий Колмогорова"].Points.AddXY(xMaxDif, LambdaRandVarGenerator.FinRange(xMaxDif, lambda, left, right));
+            listBox1.Items.Add("Критерий Колмогорова:");
+            listBox1.Items.Add(Math.Round(difMax, 5));
             #endregion
 
-            int chartSize = 100; 
-			for (int i = 0; i < chartSize; i++)
-			{
-                float step = Convert.ToSingle((right - left)/(chartSize));
-                float x = Convert.ToSingle(left + step * i);
-                float y = (float)Selection.Where(p => p <= x).Count() / Selection.Count;
-                
-                chart1.Series["Эксперементальная функция"].Points.AddXY(x, y);
-			}
-            int pocketNum = 1 + Convert.ToInt32(Math.Log(Selection.Count)/Math.Log(2));
+            #region Параметры выборки
+            listBox1.Items.Add("Медиана выборки");
+            listBox1.Items.Add(Math.Round(LambdaRandVarGenerator.GetMedian(Selection), 5));
+            listBox1.Items.Add("Мода выборки");
+            listBox1.Items.Add(Math.Round(LambdaRandVarGenerator.GetMode(Selection), 5));
+            listBox1.Items.Add("Дисперсия выборки");
+            listBox1.Items.Add(Math.Round(LambdaRandVarGenerator.GetDispersion(Selection), 5));
+            listBox1.Items.Add("Среднеквадратичное отклонение выборки");
+            listBox1.Items.Add(Math.Round(LambdaRandVarGenerator.GetStandardDeviation(Selection), 5));
+            #endregion
+            #region Критерий Хи квадрат
+            /*int pocketNum = 1 + Convert.ToInt32(Math.Log(Selection.Count) / Math.Log(2));
             double[] pocketBorder = new double[pocketNum + 1];
             double P = 1.0 / pocketNum;
             int[] Mi = new int[pocketNum];
 
             double Fa = F(left, lambda);
             double Fb = F(right, lambda);
-
-            
-			for (int i = 1; i < pocketNum; i++)
+            for (int i = 1; i < pocketNum; i++)
                 pocketBorder[i] = GenerateRandomVariable(lambda, Fa, Fb, P * i);
             pocketBorder[0] = left;
             pocketBorder[pocketNum] = right;
 
-			for (int i = 0; i < pocketNum; i++)
+            for (int i = 0; i < pocketNum; i++)
                 Mi[i] = Selection.Where(x => x >= pocketBorder[i] && x < pocketBorder[i + 1]).Count();
 
             double hiKv = 0;
-			for (int i = 0; i < pocketNum; i++)
-			{
+            for (int i = 0; i < pocketNum; i++)
+            {
                 hiKv += (Mi[i] - Selection.Count * P) * (Mi[i] - Selection.Count * P) / (Selection.Count * P);
             }
             listBox1.Items.Add("Хи квадрат: " + hiKv);
+            listBox1.Items.Add("Должно быть ~" + Math.Round(Selection.Count * P, 0));
             int index;
             for (index = 0; index < pocketNum - 1; index++)
                 listBox1.Items.Add("[" + Math.Round(pocketBorder[index], 3) + "; " + Math.Round(pocketBorder[index + 1], 3) + ") - " + Mi[index]);
 
             listBox1.Items.Add("[" + Math.Round(pocketBorder[index], 3) + "; " + Math.Round(pocketBorder[index + 1], 3) + "] - " + Mi[index]);
             listBox1.Items.Add("Итого: " + Mi.Sum());
+            */
+            #endregion
         }
-	}
-    
+        private void Start(object sender, EventArgs e)
+        {
+			try
+			{
+                GenerateAndPrint();
+			}
+			catch (Exception exception)
+			{
+                MessageBox.Show(exception.Message);
+			}
+        }
+    }
 }
---
+++ Graphic/LambdaRandVarGenerator.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Graphic
+{
+	static class LambdaRandVarGenerator
+	{
+        static public double F(double x, double lambda)
+        {
+            if (lambda >= 0)
+            {
+                if (x <= 0)
+                    return 0;
+                if (x >= 2 / lambda)
+                    return 1;
+            }
+            else
+            {
+                if (x <= 4 / lambda)
+                    return 0;
+                if (x >= 2 / lambda)
+                    return 1;
+            }
+            return lambda * x - Math.Pow(lambda, 2) * Math.Pow(x, 2) / 4;
+        }
+        static public double FinRange(double x, double lambda, double left, double right)
+		{
+            return (F(x, lambda) - F(left, lambda)) / (F(right, lambda) - F(left, lambda));
+        }
+        static public double GenerateRandomVariable(double lambda, double leftProbability, double rightProbability, double rand0_1)
+        {
+            if (rand0_1 > 1 || rand0_1 < 0)
+                throw new Exception("Параметр rand0_1 передан с недопустимым значением");
+            double r = leftProbability + rand0_1 * (rightProbability - leftProbability);
+            return 2 * (lambda - Math.Abs(lambda) * Math.Sqrt(1 - r)) / Math.Pow(lambda, 2);
+        }
+        static public List<double> GenerateSelection(int amount, double left, double right, double lambda)
+        {
+            double leftProbability, rightProbability;
+            if (right <= left)
+                throw new Exception("Правая граница меньше либо равна левой");
+
+            if (lambda > 0)
+            {
+                if (left < 0)
+                    leftProbability = 0;
+                else
+                    leftProbability = F(left, lambda);
+            }
+            else if (lambda < 0)
+            {
+                if (left < 4 / lambda)
+                    leftProbability = 0;
+                else
+                    leftProbability = F(left, lambda);
+            }
+            else
+                throw new Exception("lambda = 0");
+
+
+            if (right > 2 / lambda)
+                rightProbability = 1;
+            else
+                rightProbability = F(right, lambda);
+
+            if (leftProbability == rightProbability)
+                throw new Exception("На этом интервале не генерируется СВ");
+
+            List<double> Selection = new List<double>();
+            Random rnd = new Random();
+
+            for (int i = 0; i < amount; i++)
+            {
+                Selection.Add(GenerateRandomVariable(lambda, leftProbability, rightProbability, rnd.NextDouble()));
+            }
+            return Selection;
+        }
+        static public double GetMedian(List<double> selection)
+        {
+            if (selection.Count == 0)
+            {
+                throw new Exception("Пустая выборка!");
+            }
+            List<double> copiedSelection = selection;
+            copiedSelection.Sort();
+            if (selection.Count % 2 == 1)
+            {
+                return copiedSelection[(copiedSelection.Count + 1) / 2];
+            }
+            else
+            {
+                return (copiedSelection[copiedSelection.Count / 2] + copiedSelection[(copiedSelection.Count + 1) / 2]) / 2;
+            }
+        }
+        static public double GetMode(List<double> selection)
+        {
+            if (selection.Count == 0)
+                throw new Exception("Пустая выборка!");
+
+            Dictionary<double, int> dict = new Dictionary<double, int>();
+            foreach (double elem in selection)
+            {
+                if (dict.ContainsKey(elem))
+                    dict[elem]++;
+                else
+                    dict[elem] = 1;
+            }
+
+            int maxCount = 1;
+            double mode = Double.NaN;
+            foreach (double elem in dict.Keys)
+            {
+                if (dict[elem] > maxCount)
+                {
+                    maxCount = dict[elem];
+                    mode = elem;
+                }
+            }
+            return mode;
+        }
+        static public double GetDispersion(List<double> selection)
+        {
+            double m = selection.Average();
+            return selection.Sum(a => (a - m) * (a - m)) / (selection.Count - 1);
+        }
+        static public double GetStandardDeviation(List<double> selection)
+        {
+            return Math.Sqrt(GetDispersion(selection));
+        }
+    }
+}
